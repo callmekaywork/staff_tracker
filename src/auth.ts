405b0bd7cd5e143e2232bb2b425aae9b8bc8c8ed
callmekaywork <callmekaywork@gmail.com>
2026-01-28 13:52:19 +0200

    lets see

--- src/auth.ts
+++ src/auth.ts
@@ -1,9 +1,9 @@
-import Credentials from "next-auth/providers/credentials";
-import { encode as defaultEncode } from "next-auth/jwt";
-import NextAuth from "next-auth";
-import { drizzleAdapter } from "@/lib/drizzleAdapter";
-import authConfig from "@/lib/authConfig";
-import { v4 as uuidv4 } from "uuid";
+import Credentials from 'next-auth/providers/credentials';
+import { encode as defaultEncode } from 'next-auth/jwt';
+import NextAuth from 'next-auth';
+import { drizzleAdapter } from '@/lib/drizzleAdapter';
+import authConfig from '@/lib/authConfig';
+import { v4 as uuidv4 } from 'uuid';
 
 export const {
   handlers: { GET, POST },
@@ -11,12 +11,12 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
-  debug: process.env.NODE_ENV != "production" && true,
+  debug: process.env.NODE_ENV != 'production' && true,
   adapter: drizzleAdapter,
   ...authConfig,
   callbacks: {
     async jwt({ token, user, account }) {
-      if (account?.provider == "credentials") {
+      if (account?.provider == 'credentials') {
         token.Credentials = true;
       }
 
@@ -43,19 +43,19 @@ export const {
         // Return false to display a default error message
         // return false;
         // Or you can return a URL to redirect to:
-        return "/unauthorized";
+        return '/unauthorized';
       }
     },
     async redirect({ url, baseUrl }) {
       // Allows relative callback URLs
-      if (url.startsWith("/")) return `${baseUrl}${url}`;
+      if (url.startsWith('/')) return `${baseUrl}${url}`;
       // Allows callback URLs on the same origin
       else if (new URL(url).origin === baseUrl) return url;
       return baseUrl;
     },
   },
   session: {
-    strategy: "database",
+    strategy: 'database',
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   },
   jwt: {
@@ -64,17 +64,17 @@ export const {
         const sessionToken = uuidv4();
 
         if (!params.token.sub) {
-          throw new Error("No User Id found");
+          throw new Error('No User Id found');
         }
 
         const createdSession = await drizzleAdapter?.createSession?.({
           sessionToken: sessionToken,
-          userId: params.token.id,
+          userId: params.token.id as string,
           expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
         });
 
         if (!createdSession) {
-          throw new Error("Failed to create a session");
+          throw new Error('Failed to create a session');
         }
 
         return sessionToken;
@@ -85,12 +85,12 @@ export const {
   },
   cookies: {
     sessionToken: {
-      name: "next-auth.session-token",
+      name: 'next-auth.session-token',
       options: {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "lax",
-        path: "/",
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'lax',
+        path: '/',
         maxAge: 30 * 24 * 60 * 60, // 30 days
       },
     },
